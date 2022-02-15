d610e8c8371d7fb9f7b6b150a68abdf33bcf244e
gregoirelatour <gregoire.latour@edu.devinci.fr>
2022-02-15 09:18:11 +0100

    fix & ajout de l'objet Option

--- Algo_trading.jl
+++ Algo_trading.jl
@@ -1,16 +1,14 @@
 #using QuandlAccess
 #using Gadfly
 using AlphaVantage
-using DataFrames, Dates
+using DataFrames, Dates, Tables, TimeSeries, Statistics
 using DataFramesMeta
 using Plots
 theme(:juno)
 
-
-
 client = AlphaVantage.GLOBAL[]
 client.key = "6065Z0SK19ZA64GE"
-#6065Z0SK19ZA64GE api key alpha
+#6065Z0SK19ZA64GE api key alpha vantage
 
 function raw_to_dataframe(rawData)
     df = DataFrame(rawData[1])
@@ -38,19 +36,6 @@ function intra_to_dataframe(rawData)
     return df
 end
 
-# tslaRaw = AlphaVantage.time_series_daily("", outputsize="full")
-
-# Plots.plot(tslaRaw)
-
-# gr(size=(800,470))
-# spy = time_series_daily("SPY", outputsize="full");
-# # Convert to a DataFrame
-# data = DataFrame(spy);
-# # Convert timestamp column to Date type
-# data[!, :timestamp] = Dates.Date.(data[!, :timestamp]);
-# data[!, :open] = Float64.(data[!, :open])
-# # Plot the timeseries
-# Plots.plot(data[!, :timestamp], data[!, :open], label=["Open"])
 function returns_dataframe(data)
     y = Vector{Float64}(undef, length(data."open"))
     for i in range(1,length(data."open")-1)
@@ -70,6 +55,55 @@ function cumulative_returns_dataframe(data)
     data."cumu_returns"= y
 end
 
+function mov_mean(data, min, max)
+    res = Vector{Float64}(undef, length(data))
+    for i in range(min + 1, length(data)-max)
+        res[i]=mean(data[i-min: i+max])
+    end
+    
+    return res
+end
+
+# function LeeMykland(data, delta, significance_level = 0.01)
+#     tm = 252*24*60 #utile en 1 minute
+#     k = ceil(sqrt(tm/delta))
+#     r = append!(diff(log.(data)))
+#     bpv = abs.(r) .* abs.(reverse(r))
+#     bpv = 
+
+    
+# end
+
+#Creation of the Option object
+mutable struct Option
+    market
+    type
+    open #Days
+    close
+    returns
+
+    function Option(market,type, open, close)
+        this = new()
+        this.type= type
+        this.open = open
+        this.close = close
+
+        o = values(market[open])[1]
+        c = values(market[close])[1]
+        
+
+        if type == "SHORT"
+            this.returns = -(c/o-1)
+        elseif type == "LONG"
+            this.returns = c/o-1
+
+        end
+
+        return this
+        
+    end
+
+end
 # function pre_avrage_log_returns(data)
 #     y = Vector{Float64}(undef, length(data."open"))
 #     y[1]= 0.0
@@ -79,20 +113,34 @@ end
 #     for i in range(2, length(data.open))
 #         for j in range (2, i)
 #             t = log(y[i-K+j])
-
-
     
 # end
 eurusdRaw = AlphaVantage.fx_daily("EUR", "JPY",outputsize="full")
 EURUSD = DataFrame(eurusdRaw)
 EURUSD[!, :timestamp] = Dates.Date.(EURUSD[!, :timestamp])
+timearr = TimeArray(EURUSD, timestamp = :timestamp)
 
-returns_dataframe(EURUSD)
-cumulative_returns_dataframe(EURUSD)
-Plots.plot(EURUSD[!, :timestamp], EURUSD[!, :open], label=["Open"], dpi=500,thickness_scaling=1, minorgrid = true)
+data_open = timearr[:open]
+#x = data_open[Date(2002,12,16)] - data_open[Date(2002,12,20)]
+y = Vector{Float64}(undef, length(EURUSD[!, :open]))
+y = mov_mean(EURUSD[!, :open],0,10)
+
+short1 = Option(data_open,"SHORT", Date(2022,02,03),Date(2022,02,09))
+long1 = Option(data_open,"LONG", Date(2022,02,03),Date(2022,02,09))
+
+println(short1.returns," ", long1.returns)
+#Plots.plot(y)
+# returns_dataframe(EURUSD)
+# cumulative_returns_dataframe(EURUSD)
 
 
-###QUANDL####
+
+
+#Plot
+# Plots.plot(EURUSD[!, :timestamp], EURUSD[!, :open], label=["Open"], dpi=500,thickness_scaling=1, minorgrid = true)
+
+
+###test avec l'api de QUANDL####
 #quandl = Quandl("wTCUEtx5HUzJFV28QAQq")
 
 #test=quandl(TimeSeries("ML/BBY"))
